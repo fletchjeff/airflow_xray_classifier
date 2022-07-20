0afc899d42eb8d7c9858b364b0a8a7a3ff568d6b
fletchjeff <fletch.jeff@gmail.com>
2022-07-20 20:51:41 +0200

    Updates

--- include/code/transfer_learning-2_class.py
+++ include/code/transfer_learning-2_class.py
@@ -3,7 +3,6 @@ import sys
 import pathlib
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-#import datetime
 import os
 import mlflow
 
@@ -17,8 +16,8 @@ data_dir = pathlib.Path("{}/data/train/".format(data_path_args))
 batch_size = 32
 img_height = 224
 img_width = 224
-img_size = 224
-IMG_SIZE = (224, 224)
+#img_size = 224
+img_size = (224, 224)
 
 
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
@@ -56,55 +55,46 @@ validation_dataset = validation_dataset.skip(val_batches // 5)
 print('Number of validation batches: %d' % tf.data.experimental.cardinality(validation_dataset))
 print('Number of test batches: %d' % tf.data.experimental.cardinality(test_dataset))
 
+
 # Configure the dataset for performance
-
 AUTOTUNE = tf.data.AUTOTUNE
-
 train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
 validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
 test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
 
-# Implement data augmentation
-
+# Data augmentation
 data_augmentation = tf.keras.Sequential([
-  tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal", 
-                                                 input_shape=(img_height, 
-                                                              img_width,
-                                                              3)),
-  tf.keras.layers.experimental.preprocessing.RandomRotation(0.05),
-  tf.keras.layers.experimental.preprocessing.RandomZoom(0.05),
+  tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(img_height, img_width,3)),
+  tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
+  tf.keras.layers.experimental.preprocessing.RandomZoom(0.1),
 ])
 
+# Rescale pixel values
 preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+img_shape = img_size + (3,)
 
-# Create the base model from the pre-trained model MobileNet V2
-IMG_SHAPE = IMG_SIZE + (3,)
-base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
-                                               include_top=False,
-                                               weights='imagenet')
-
+# Load base model
+base_model = tf.keras.applications.MobileNetV2(
+  input_shape=img_shape,
+  include_top=False,
+  weights='imagenet'
+)
 
 image_batch, label_batch = next(iter(train_dataset))
 feature_batch = base_model(image_batch)
 print(feature_batch.shape)
 
+## Feature extraction
+# Freeze the convolutional base
 base_model.trainable = False
 
-# Show the base model
-base_model.summary()
-
-
+# Add a classification head
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 feature_batch_average = global_average_layer(feature_batch)
-print(feature_batch_average.shape)
-
-
-prediction_layer = tf.keras.layers.Dense(3,activation='softmax')
+prediction_layer = tf.keras.layers.Dense(1)
 prediction_batch = prediction_layer(feature_batch_average)
-print(prediction_batch.shape)
 
-
-
+# Build model
 inputs = tf.keras.Input(shape=(224, 224, 3))
 x = data_augmentation(inputs)
 x = preprocess_input(x)
@@ -115,90 +105,68 @@ outputs = prediction_layer(x)
 model = tf.keras.Model(inputs, outputs)
 
 
-
+# Compile the model
 base_learning_rate = 0.0001
-model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-              optimizer = tf.keras.optimizers.RMSprop(learning_rate=base_learning_rate),
-              metrics=['accuracy'])
+model.compile(
+  optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+  metrics=['accuracy']
+)
 
-model.summary()
-
-len(model.trainable_variables)
-
-
+# Train the model
 initial_epochs = 10
-
 loss0, accuracy0 = model.evaluate(validation_dataset)
-
 print("initial loss: {:.2f}".format(loss0))
 print("initial accuracy: {:.2f}".format(accuracy0))
 
-#log_dir = "{}/xray/training_runs/logs/fit/{}".format(data_path_args,run_date)
-
-#tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
+# Start mlflow
 mlflow.set_tracking_uri("http://a8a7d3412ef1349b38919a29cce0d563-995607172.eu-central-1.elb.amazonaws.com:5000")
 mlflow.keras.autolog(registered_model_name=f"xray_model_train_{run_date}")
 
-history = model.fit(train_dataset,
-                    epochs=initial_epochs,
-                    validation_data=validation_dataset)
-                    #callbacks=[tensorboard_callback])
+history = model.fit(
+  train_dataset,
+  epochs=initial_epochs,
+  validation_data=validation_dataset
+)
 
+# Record metrics
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
-
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-
-
-
+## Fine tuning
+# Un-freeze the top layers of the model
 base_model.trainable = True
-
-
-
-# Let's take a look to see how many layers are in the base model
-print("Number of layers in the base model: ", len(base_model.layers))
-
-# Fine-tune from this layer onwards
 fine_tune_at = 100
-
-# Freeze all the layers before the `fine_tune_at` layer
 for layer in base_model.layers[:fine_tune_at]:
   layer.trainable =  False
 
-# ### Compile the model
-# 
-# As you are training a much larger model and want to readapt the pretrained weights, it is important to use a lower learning rate at this stage. Otherwise, your model could overfit very quickly.
+# Re-compile the model
+model.compile(
+  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+  optimizer = tf.keras.optimizers.RMSprop(lr=base_learning_rate/10),
+  metrics=['accuracy']
+)
 
-model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-              optimizer = tf.keras.optimizers.RMSprop(learning_rate=base_learning_rate/10),
-              metrics=['accuracy'])
-
-
-model.summary()
-
-len(model.trainable_variables)
-
-fine_tune_epochs = 20
+# Continue training the model
+fine_tune_epochs = 15
 total_epochs =  initial_epochs + fine_tune_epochs
-
-history_fine = model.fit(train_dataset,
-                         epochs=total_epochs,
-                         initial_epoch=history.epoch[-1],
-                         validation_data=validation_dataset)
-                         #callbacks=[tensorboard_callback])
+history_fine = model.fit(
+  train_dataset,
+  epochs=total_epochs,
+  initial_epoch=history.epoch[-1],
+  validation_data=validation_dataset
+)
 
 acc += history_fine.history['accuracy']
 val_acc += history_fine.history['val_accuracy']
-
 loss += history_fine.history['loss']
 val_loss += history_fine.history['val_loss']
 
 loss, accuracy = model.evaluate(test_dataset)
 print('Test accuracy :', accuracy)
 
+# Save the model
 os.mkdir('{}/models/{}/'.format(data_path_args,run_date))
-
 model.save("{}/models/{}/xray_classifier_model.h5".format(data_path_args,run_date))
